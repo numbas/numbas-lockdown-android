152841213b18f51fcc3f3e2b1985d2b649e1d4ff
Lycanic <17724612+Lycanic@users.noreply.github.com>
2024-09-06 14:09:15 +0100

    refactor ID

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.numbasapp"
+        applicationId = "uk.org.numbas.numbasapp"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
